c0826e7d212e141d245d9b1a5d6fe4801d363dc3
U1 <ewanjamesw@outlook.com>
2024-12-04 15:58:41 +0000

    edits to UI

--- index.php
+++ index.php
@@ -12,12 +12,12 @@
     <header class="p-3 mb-3 border-bottom">
         <div class="container" bis_skin_checked="1">
             <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start" bis_skin_checked="1">
-            <a href="/index.php" class="d-flex align-items-center mb-2 mb-lg-0 link-body-emphasis text-decoration-none">
+                <a href="/index.php" class="d-flex align-items-center mb-2 mb-lg-0 link-body-emphasis text-decoration-none">
                     <img class="bi me-2" height="50px" role="img" aria-label="Bootstrap" src="/img/logo.PNG">
                 </a>
 
                 <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-                    <li><a href="#" class="nav-link px-2 link-body-emphasis">Home</a></li>
+                    <li><a href="#" class="nav-link px-2 link-secondary">Home</a></li>
                     <li><a href="#" class="nav-link px-2 link-body-emphasis">Book service</a></li>
                     <li><a href="#" class="nav-link px-2 link-body-emphasis">Inquire</a></li>
                     <li><a href="#" class="nav-link px-2 link-body-emphasis">Previous bookings</a></li>
@@ -27,7 +27,7 @@
                     <a href="#" class="d-block link-body-emphasis text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
                         <img src="/img/profile.png" alt="mdo" width="32" height="32" class="rounded-circle">
                     </a>
-                    <ul class="dropdown-menu text-small" style="">
+                    <ul class="dropdown-menu text-small">
                         <li><a class="dropdown-item" href="#">Settings</a></li>
                         <li><a class="dropdown-item" href="#">Profile</a></li>
                         <li>
@@ -39,9 +39,6 @@
             </div>
         </div>
     </header>
-
-<!--     <li><a href="#" class="nav-link px-2 link-secondary">Home</a></li>
- -->
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
