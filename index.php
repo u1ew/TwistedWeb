36d24edb9a4121ae0a4a486f841918ce2d8960da
U1 <ewanjamesw@outlook.com>
2024-12-04 15:44:34 +0000

    UI update

--- index.php
+++ index.php
@@ -1,21 +1,49 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Home Page</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
-<body>
-    <header>
-        <h1>The Car Garage Company</h1>
-        <nav>
-            <a href="#">Home</a>
-            <a href="#">Book service</a>
-            <a href="#">Inquire</a>
-            <a href="#">Previous bookings</a>
-        </nav>
 
+<body>
+    <header class="p-3 mb-3 border-bottom">
+        <div class="container" bis_skin_checked="1">
+            <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start" bis_skin_checked="1">
+            <a href="/index.php" class="d-flex align-items-center mb-2 mb-lg-0 link-body-emphasis text-decoration-none">
+                    <img class="bi me-2" height="50px" role="img" aria-label="Bootstrap" src="/img/logo.PNG">
+                </a>
+
+                <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+                    <li><a href="#" class="nav-link px-2 link-body-emphasis">Home</a></li>
+                    <li><a href="#" class="nav-link px-2 link-body-emphasis">Book service</a></li>
+                    <li><a href="#" class="nav-link px-2 link-body-emphasis">Inquire</a></li>
+                    <li><a href="#" class="nav-link px-2 link-body-emphasis">Previous bookings</a></li>
+                </ul>
+
+                <div class="dropdown text-end" bis_skin_checked="1">
+                    <a href="#" class="d-block link-body-emphasis text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                        <img src="/img/profile.png" alt="mdo" width="32" height="32" class="rounded-circle">
+                    </a>
+                    <ul class="dropdown-menu text-small" style="">
+                        <li><a class="dropdown-item" href="#">Settings</a></li>
+                        <li><a class="dropdown-item" href="#">Profile</a></li>
+                        <li>
+                            <hr class="dropdown-divider">
+                        </li>
+                        <li><a class="dropdown-item" href="#">Sign out</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
     </header>
-    
+
+<!--     <li><a href="#" class="nav-link px-2 link-secondary">Home</a></li>
+ -->
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
+
 </html>
